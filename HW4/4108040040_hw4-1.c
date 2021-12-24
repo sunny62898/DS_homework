7b614957da883a6423c2a57e66e956d124451d55
sunny62898 <85967575+sunny62898@users.noreply.github.com>
2021-12-25 01:03:51 +0800

    finish 4-1

--- HW4/4108040040_hw4-1.c
+++ HW4/4108040040_hw4-1.c
@@ -9,10 +9,13 @@ typedef struct node{
 
 struct headNode{
 	int parent;		//紀錄做他之前要先做的數量
+	int pass;
 	nodePointer next;	//連到node 
 };
 
+
 struct headNode connect(struct headNode, int);
+void calParent(struct headNode*, struct headNode);
 
 
 int main(){
@@ -21,20 +24,20 @@ int main(){
 	fp = fopen("input_1.txt", "r");
 	
 	int jobNum, relation;
-	int i;
+	int i, j;
 	
 	fscanf(fp, "%d", &jobNum);
 	fscanf(fp, "%d", &relation);
 	
-	printf("%d %d\n", jobNum, relation);
-	
 	/*job從1開始 所以設job[0]為空(不使用)*/
 	struct headNode job[jobNum+1];	//建立每個job的head
+	struct headNode* jobPointer = job;
 	
 	
 	/*設定初始值*/
 	for(i = 1;i < jobNum+1;i++){
 		job[i].parent = 0;
+		job[i].pass = 0;
 		job[i].next = NULL;
 	}
 	
@@ -43,21 +46,41 @@ int main(){
 	for(i = 0;i < relation;i++){
 		fscanf(fp, "%d", &before);
 		fscanf(fp, "%d", &after);
-		
-		printf("%d %d\n", before, after);
 	
 		job[before] = connect(job[before], after);
 		job[after].parent += 1;
 		
 	}
 	
-	/*test print*/ 
-	for(i = 1;i < jobNum+1;i++){
-		if(job[i].next != NULL){
-			printf("%d = %d\n", i, job[i].next->number);
+	int ans[jobNum];	//存答案的array 
+	int ansNum = 0;
+	int cycle = 1;
+	for(i = 0;i < jobNum;i++){
+		for(j = 1;j < jobNum+1;j++){
+			if(job[j].parent == 0 && job[j].pass == 0){		//前面沒有事情要做且沒走過 
+				job[j].pass = 1;	//標示走過
+				ans[ansNum] = j;
+				ansNum++;
+				calParent(jobPointer, job[j]);
+				cycle = 0;
+				break;
+			}
+			else{
+				cycle = 1;
+			}
 		}
-		printf("%d = %d\n", i, job[i].parent);
+		if(cycle == 1){
+			printf("not exist");
+			break;
+		}
 	}
+	
+	if(cycle != 1){
+		for(i = 0;i < jobNum;i++){
+			printf("%d ", ans[i]);
+		}
+	}
+	printf("\n");
 	
 	
 	system("pause");
@@ -84,6 +107,14 @@ struct headNode connect(struct headNode head, int num){
 	}
 	
 	return head;
+}
+
+void calParent(struct headNode* array, struct headNode head){
+	nodePointer now = head.next;
+	while(now != NULL){
+		array[now->number].parent -= 1; 
+		now = now->next;
+	}
 }
 
 
