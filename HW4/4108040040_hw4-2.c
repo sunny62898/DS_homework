56fe2e012bed35184743f8da603fc5683ad6e056
sunny62898 <85967575+sunny62898@users.noreply.github.com>
2021-12-28 04:12:44 +0800

    地圖建立完成

--- HW4/4108040040_hw4-2.c
+++ HW4/4108040040_hw4-2.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+typedef struct node *nodePointer;
+typedef struct node{
+	int number;
+	nodePointer next;
+};
+
+struct dot{
+	int pass;
+	nodePointer next;
+};
+
+void createMap(struct dot*, int, int);
+void test(struct dot*, int);
+
 int main(){
 
 	FILE *fp;
@@ -13,11 +27,22 @@ int main(){
 	printf("%d %d\n", pointNum, orderNum);
 	
 	int i;
+	struct dot point[pointNum+1];
+	struct dot *pointer = &point;
+
+	/*ªì©l°}¦C*/
+	for(i = 1;i < pointNum+1;i++){
+		point[i].next = NULL;
+		point[i].pass = 0;
+	}
+	
+
 	int source, dist, cost;
 	
 	for(i = 0;i < pointNum-1;i++){
 		fscanf(fp, "%d %d %d", &source, &dist, &cost);
 		printf("%d %d %d\n", source, dist, cost);
+		createMap(pointer, source, dist);
 	}
 	
 	int outset, number;
@@ -26,7 +51,9 @@ int main(){
 		printf("%d %d\n", outset, number);
 	}
 	 
-	
+	/*test print*/
+
+	test(pointer, pointNum+1);
 	
 	
 	fclose(fp);	
@@ -34,3 +61,52 @@ int main(){
     system("pause");
     return 0;
 }
+
+
+void createMap(struct dot* array, int num1, int num2){
+
+	nodePointer new1 = (nodePointer)malloc(sizeof(struct node));
+	nodePointer new2 = (nodePointer)malloc(sizeof(struct node));
+	new1->number = num1;
+	new2->number = num2;
+	new1->next = new2->next = NULL;
+	
+	if(array[num1].next == NULL){
+		array[num1].next = new2;
+	}
+	else{
+		nodePointer now1 = array[num1].next;
+		while(now1->next != NULL){
+			now1 = now1->next;
+		}
+		now1->next = new2;
+	}
+	
+	
+	if(array[num2].next == NULL){
+		array[num2].next = new1;
+	}
+	else{
+		nodePointer now2 = array[num2].next;
+		while(now2->next != NULL){
+			now2 = now2->next;
+		}
+		now2->next = new1;
+	}
+}
+
+void test(struct dot* array, int num){
+	int i;
+	nodePointer now = array[0].next;
+	for(i = 1;i < num;i++){
+		now = array[i].next;
+		printf("%d = ", i);
+		while(now != NULL){
+			printf("%d ", now->number);
+			now = now->next;
+		}
+		printf("\n");
+	}
+}
+
+
